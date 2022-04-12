f8d139380a7d76006c6df25ee3a6b2a440086ea7
unknown <manh87835@gmail.com>
2022-04-12 15:04:45 +0700

    perfect

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -3,11 +3,11 @@ import java.io.FileReader;
 
 public class ReadFile {
     public static void main(String[] args) {
-       String file="src\\bang1.csv";
+       String file1="src\\bang1.csv";
        BufferedReader bufferedReader=null;
        String s="";
        try {
-           bufferedReader=new BufferedReader(new FileReader(file));
+           bufferedReader=new BufferedReader(new FileReader(file1));
            while ((s=bufferedReader.readLine())!=null){
 //               System.out.println(s);
                String[] arr= s.split(",");
